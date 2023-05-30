fe811f7ca9cdda651f67abde7a4161f3260a4b8a
2moe <m@tmoe.me>
2023-05-30 21:08:51 +0800

    feat(highlight): Use `hlight` instead of `syntect`

--- codegen/src/highlight.rs
+++ codegen/src/highlight.rs
@@ -1,14 +1,8 @@
-use crate::highlight::syntax::static_syntax_set;
 use getset::{Getters, MutGetters};
-use once_cell::sync::OnceCell;
+use hlight::HighLightRes;
 use std::{borrow::Cow, collections::HashMap, ffi::OsStr};
-pub use syntect::{dumps, highlighting::ThemeSet};
-use syntect::{highlighting::Theme, parsing::SyntaxSet};
-pub mod output;
-mod syntax;
-pub mod theme;
 
-#[derive(Getters, MutGetters, Debug, Clone)]
+#[derive(Getters, MutGetters, Debug)]
 #[getset(get = "pub with_prefix", get_mut = "pub with_prefix")]
 pub struct HighLight<'res, 'p> {
     resource: HighLightRes<'res>,
@@ -22,16 +16,6 @@ impl<'res, 'p> HighLight<'res, 'p> {
     ) -> Self {
         Self { resource, files }
     }
-}
-
-#[derive(Getters, MutGetters, Debug, Clone)]
-#[getset(get = "pub with_prefix", get_mut = "pub with_prefix")]
-pub struct HighLightRes<'name> {
-    name: Cow<'name, str>,
-    theme: OnceCell<Theme>,
-    syntax_set: &'static SyntaxSet,
-    theme_set: &'name ThemeSet,
-    background: bool,
 }
 
 #[derive(Getters, MutGetters, Debug, Clone)]
@@ -59,37 +43,6 @@ impl<'p> HighLightFmt<'p> {
             syntax: fmt,
             suffix: prefix,
             ..Default::default()
-        }
-    }
-}
-
-impl<'name> HighLightRes<'name> {
-    pub fn new(name: Cow<'name, str>, theme_set: &'name ThemeSet) -> Self {
-        Self {
-            name,
-            theme_set,
-            syntax_set: static_syntax_set(),
-            ..Default::default()
-        }
-    }
-
-    /// Enable or disable background
-    pub fn with_background(self, switch: bool) -> Self {
-        Self {
-            background: switch,
-            ..self
-        }
-    }
-}
-
-impl<'name> Default for HighLightRes<'name> {
-    fn default() -> Self {
-        Self {
-            name: Cow::from(Self::monokai_theme_name()),
-            theme: OnceCell::new(),
-            syntax_set: static_syntax_set(),
-            theme_set: Self::static_theme_set(),
-            background: true,
         }
     }
 }
