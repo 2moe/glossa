fcdab290306f90b71bcb9f48bac8be23cae30fda
2moe <m@tmoe.me>
2025-04-05 07:46:17 +0800

    test: add cargo_pub

---
+++ tests/cargo_pub.rs
@@ -0,0 +1,27 @@
+use std::{env::set_current_dir, path::Path, process::Command};
+
+use anyhow::Result as AnyResult;
+use tap::Pipe;
+
+#[ignore]
+#[test]
+fn cargo_publish() -> AnyResult<()> {
+  let workdir = env!("CARGO_MANIFEST_DIR").pipe(Path::new);
+
+  [
+    "crates/codegen",
+    // "crates/glossa",
+    // "crates/shared",
+    // "crates/l10n",
+    // "crates/cli",
+    // "crates/ui",
+  ]
+  .into_iter()
+  .try_for_each(|d| {
+    workdir
+      .join(d)
+      .pipe(set_current_dir)?;
+    Command::new("cargo-pub").status()?;
+    Ok(())
+  })
+}
