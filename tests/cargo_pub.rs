a90a6324589856a92cc0f2ec3dd79b9257f9d445
2moe <m@tmoe.me>
2025-04-24 22:51:34 +0800

    release(glossa-codegen): v0.0.2

--- tests/cargo_pub.rs
+++ tests/cargo_pub.rs
@@ -9,8 +9,8 @@ fn cargo_publish() -> AnyResult<()> {
   let workdir = env!("CARGO_MANIFEST_DIR").pipe(Path::new);
 
   [
-    // "crates/codegen",
-    "crates/glossa",
+    "crates/codegen",
+    // "crates/glossa",
     // "crates/shared",
     // "crates/l10n",
     // "crates/cli",
