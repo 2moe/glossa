3a642282c175c7c8f5a5b3de30947bfa3d3a4214
2moe <m@tmoe.me>
2025-05-30 00:48:57 +0800

    ci(digest): add calculate_and_output_digests

--- ci/calculate_digest.rb
+++ ci/calculate_digest.rb
@@ -4,37 +4,52 @@ require 'json'
 require 'digest/sha2'
 require 'digest/blake3'
 
-# The **release** directory is already created during the execution of docker.rb, so we can proceed to enter it directly at this point
-Dir.chdir 'release'
-digests = {}
-Pathname.glob('*.zst').each do |path|
-  file_name = path.basename
-  sha256 = Digest::SHA256.file(path).hexdigest
-  blake3 = Digest::Blake3.file(path).hexdigest
+def calculate_and_output_digests(glob_list = '*.zst', workdir = 'release')
+  Pathname(workdir)
+    .then { _1.mkpath unless _1.exist? }
+    .then { Dir.chdir _1 }
 
-  digests[file_name] = {
-    file_size: path.size?,
-    digest: {
-      sha256: sha256,
-      blake3: blake3
+  digests = calculate_digests(glob_list)
+  create_markdown_table(digests)
+  generate_digests_json(digests)
+end
+
+def calculate_digests(file_glob_list = '*.zst')
+  digests = {}
+  Pathname.glob(file_glob_list).each do |path|
+    file_name = path.basename
+    sha256 = Digest::SHA256.file(path).hexdigest
+    blake3 = Digest::Blake3.file(path).hexdigest
+
+    digests[file_name] = {
+      file_size: path.size?,
+      digest: {
+        sha256: sha256,
+        blake3: blake3
+      }
     }
-  }
+  end
+  digests
 end
 
-# mutable String
-markdown = String.new "| File   | Size (bytes)  | SHA256 | Blake3 |\n"
-markdown <<           "|--------|---------------|--------|--------|\n"
+def create_markdown_table(digests)
+  # mutable String
+  markdown = String.new "| File   | Size (bytes)  | SHA256 | Blake3 |\n"
+  markdown <<           "|--------|---------------|--------|--------|\n"
 
-digests.each do |file, info|
-  digest = info[:digest]
-  sha256 = digest[:sha256]
-  blake3 = digest[:blake3]
-  size   = info[:file_size]
+  digests.each do |file, info|
+    digest = info[:digest]
+    sha256 = digest[:sha256]
+    blake3 = digest[:blake3]
+    size   = info[:file_size]
 
-  markdown << "| #{file} | #{size} | `#{sha256}` | `#{blake3}` |\n"
+    markdown << "| #{file} | #{size} | `#{sha256}` | `#{blake3}` |\n"
+  end
+  File.write('digests.md', markdown)
 end
 
-File.write('digests.md', markdown)
 
-JSON.pretty_generate(digests)
-    .then { File.write('digests.json', _1) }
+def generate_digests_json(digests)
+  JSON.pretty_generate(digests)
+      .then { File.write('digests.json', _1) }
+end
--- ci/install_gem.rb
+++ ci/install_gem.rb
@@ -3,5 +3,5 @@ def install_gem(req: 'digest/blake3', gem_name: 'blake3-rb')
 rescue LoadError => e
   p e
   %w[gem install --user-install].push(gem_name).then(&run)
-  # File.write(ENV['GITHUB_PATH'], "#{Gem.user_dir}/bin\n", mode: 'a')
+  File.write(ENV['GITHUB_PATH'], "#{Gem.user_dir}/bin\n", mode: 'a')
 end
